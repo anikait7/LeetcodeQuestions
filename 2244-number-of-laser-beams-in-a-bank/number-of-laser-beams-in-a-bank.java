60a3ef9857dee135958f875c9bd3bbbc0d558472
anikait7 <anikaitchitlangia1910@gmail.com>
2025-10-27 12:01:47 +0530

    Time: 12 ms (53.73%) | Memory: 45.4 MB (35.36%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,31 @@
+class Solution 
+{
+    public int numberOfBeams(String[] bank) 
+    {
+        int count=0;
+        int prev=0;
+        int arr[] = new int[bank.length];
+
+        for(int i=0;i<bank.length;i++)
+        {
+            int c=0;
+            for(int j=0;j<bank[i].length();j++)
+                if(bank[i].charAt(j)=='1')
+                    c++;
+            
+            if(c==0)
+                continue;
+
+            if(prev==0)
+            {
+                prev=c;
+                continue;
+            }
+
+            count+=prev*c;
+            prev=c;
+        }
+
+        return count;
+    }
+}
