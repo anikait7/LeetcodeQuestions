328ff4a4325b45f5a7a136646741247a5d3b1b11
anikait7 <anikaitchitlangia1910@gmail.com>
2025-11-17 05:39:49 +0530

    Time: 1 ms (99.71%) | Memory: 65.7 MB (5.71%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public boolean kLengthApart(int[] nums, int k) 
+    {
+        int prev=-10001;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                if(i-prev<=k)
+                    return false;
+
+                prev=i;
+            }
+        }
+
+        return true;
+    }
+}
