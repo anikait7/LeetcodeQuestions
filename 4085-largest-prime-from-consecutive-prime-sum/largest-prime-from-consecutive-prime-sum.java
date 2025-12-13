2e5a74f18d051a226580bd6696277758a359ae4d
anikait7 <anikaitchitlangia1910@gmail.com>
2025-12-14 01:40:21 +0530

    Time: 826 ms (10.42%) | Memory: 269.1 MB (5.05%) - LeetSync

---
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.java
@@ -0,0 +1,41 @@
+class Solution 
+{
+    public int largestPrime(int n) 
+    {
+        List<Integer> prime = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        boolean flag[] = new boolean[n+1];
+        Arrays.fill(flag,true);
+
+        for(int i=2;i<=n;i++)
+        {
+            if(flag[i])
+            {
+                for(int j=i*2;j<=n;j+=i)
+                    flag[j]=false;
+
+                set.add(i);
+                prime.add(i);
+            }
+        }
+
+        int pre[] = new int[prime.size()+1];
+        for(int i=0;i<prime.size();i++)
+            pre[i+1]=pre[i]+prime.get(i);
+
+        int max=0;
+        for(int j=1;j<pre.length;j++)
+        {
+            int sum=pre[j]-pre[0];
+            if(set.contains(sum) && sum>max)
+                max=sum;
+
+            if(sum>n)
+                break;
+        }
+        
+
+        return max;
+    }
+}
