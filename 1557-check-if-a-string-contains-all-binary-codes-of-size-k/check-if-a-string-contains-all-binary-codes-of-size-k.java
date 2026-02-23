6b924ea9fd5010fc9e76e91717f3552aa1fc0f43
anikait7 <anikaitchitlangia1910@gmail.com>
2026-02-23 08:44:01 +0530

    Time: 176 ms (38.27%) | Memory: 96.6 MB (56.30%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -2,31 +2,34 @@ class Solution
 {
     List<String> list = new ArrayList<>();
 
-    public void compute(String s, int k)
-    {
-        if(s.length()==k)
-        {
-            list.add(s);
-            return ;
-        }
+    // public void compute(String s, int k)
+    // {
+    //     if(s.length()==k)
+    //     {
+    //         list.add(s);
+    //         return ;
+    //     }
 
-        compute(s+"0",k);
-        compute(s+"1",k);
-    }
+    //     compute(s+"0",k);
+    //     compute(s+"1",k);
+    // }
 
     public boolean hasAllCodes(String s, int k) 
     {
-        compute("",k);
+        // compute("",k);
         Set<String> set = new HashSet<>();
         for(int i=0;i<=s.length()-k;i++)
             set.add(s.substring(i,i+k));
 
-        for(int i=0;i<list.size();i++)
-        {
-            if(!set.contains(list.get(i)))
-                return false;
-        }
+        // for(int i=0;i<list.size();i++)
+        // {
+        //     if(!set.contains(list.get(i)))
+        //         return false;
+        // }
 
-        return true;
+        if(set.size()==Math.pow(2,k))
+            return true;
+
+        return false;
     }
 }
