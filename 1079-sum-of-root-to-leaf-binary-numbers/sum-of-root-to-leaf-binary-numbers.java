f1339f4c2b83ba355652b4d7b757af3e92b7bdcc
anikait7 <anikaitchitlangia1910@gmail.com>
2026-02-24 11:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (28.76%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -16,44 +16,21 @@
 
 class Solution 
 {
-    List<String> list = new ArrayList<>();
-
-    public void compute(TreeNode root, StringBuilder sb)
+    public int compute(TreeNode root, int depth)
     {
+        if(root==null)
+            return 0;
+
+        depth = (depth << 1) | root.val;
+
         if(root.left==null && root.right==null)
-        {
-            sb.append(root.val);
-            list.add(sb.toString());
-            sb.deleteCharAt(sb.length()-1);
-
-            return ;
-        }
-
-        sb.append(root.val);
-
-        if(root.left!=null)
-            compute(root.left,sb);
-
-        if(root.right!=null)
-            compute(root.right,sb);
-
-        sb.deleteCharAt(sb.length()-1);
+            return depth;
+        
+        return compute(root.left,depth) + compute(root.right,depth);
     }
 
     public int sumRootToLeaf(TreeNode root) 
     {
-        compute(root, new StringBuilder());
-        int sum=0;
-
-        for(int i=0;i<list.size();i++)
-        {
-            int c=0;  
-            for(int j=list.get(i).length()-1;j>=0;j--)
-            {
-                sum+=(list.get(i).charAt(j)-'0')*Math.pow(2,c++);
-            }
-        }
-
-        return sum;
+        return compute(root,0);
     }
 }
