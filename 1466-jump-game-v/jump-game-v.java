a39d8d4864a0e7de9876730c6f9bcdb5bedb8eaa
anikait7 <anikaitchitlangia1910@gmail.com>
2026-02-25 20:04:38 +0530

    Time: 12 ms (25.69%) | Memory: 46.5 MB (65.28%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.java
@@ -0,0 +1,50 @@
+class Solution 
+{
+    int dp[];
+
+    public int recursive(int arr[], int d, int index)
+    {
+        if(index<0 || index>=arr.length)
+            return 0;
+
+        if(dp[index]!=-1)
+            return dp[index];
+
+        int x=1;
+        int y=1;
+
+        for(int i=index+1;i<=Math.min(arr.length-1,index+d);i++)
+        {
+            if(arr[index]>arr[i])
+                x = Math.max(x,1+recursive(arr,d,i));
+            else
+                break;
+        }
+
+        for(int i=index-1;i>=Math.max(0,index-d);i--)
+        {
+            if(arr[index]>arr[i])
+                y = Math.max(y,1+recursive(arr,d,i));
+            else
+                break;
+        }
+
+        return dp[index]=Math.max(x,y);
+    }
+
+    public int maxJumps(int[] arr, int d) 
+    {
+        int n = arr.length;
+        dp = new int[n];
+        Arrays.fill(dp,-1);
+
+        for(int i=0;i<n;i++)
+            dp[i]=Math.max(dp[i],recursive(arr, d, i));
+
+        int max=0;
+        for(int i : dp)
+            max=Math.max(max,i);
+
+        return max;
+    }
+}
