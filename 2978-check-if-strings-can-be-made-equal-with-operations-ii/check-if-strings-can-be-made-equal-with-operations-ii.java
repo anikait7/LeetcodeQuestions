61e3c0ae3f2e91fa82cf35f1b5447d700313d700
anikait7 <anikaitchitlangia1910@gmail.com>
2026-03-31 01:23:01 +0530

    Time: 5 ms (93.81%) | Memory: 47.8 MB (86.60%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -2,39 +2,21 @@ class Solution
 {
     public boolean checkStrings(String s1, String s2) 
     {
-        List<Integer> even1 = new ArrayList<>();
-        List<Integer> even2 = new ArrayList<>();
-
-        List<Integer> odd1 = new ArrayList<>();
-        List<Integer> odd2 = new ArrayList<>();
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
 
         for(int i=0;i<s1.length();i++)
         {
             if(i%2==0)
             {
-                even1.add(s1.charAt(i)-'a');
-                even2.add(s2.charAt(i)-'a');
+                freq1[s1.charAt(i)-'a']++;
+                freq1[s2.charAt(i)-'a']--;
             }
             else
             {
-                odd1.add(s1.charAt(i)-'a');
-                odd2.add(s2.charAt(i)-'a');
+                freq2[s1.charAt(i)-'a']++;
+                freq2[s2.charAt(i)-'a']--;
             }
-        }
-
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
-
-        for(int i=0;i<even1.size();i++)
-        {
-            freq1[even1.get(i)]++;
-            freq1[even2.get(i)]--;
-        }
-
-        for(int i=0;i<odd1.size();i++)
-        {
-            freq2[odd1.get(i)]++;
-            freq2[odd2.get(i)]--;
         }
 
         for(int i=0;i<26;i++)
