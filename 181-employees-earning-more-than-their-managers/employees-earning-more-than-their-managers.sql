3009a5a2250ec29181ad9fadcecceb3317799dd4
anikait7 <anikaitchitlangia1910@gmail.com>
2026-05-21 19:10:46 +0530

    Time: 1260 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select e.name as Employee from Employee e
-where e.managerId is not Null and e.salary > (
+where e.managerId >=0 and e.salary > (
     select a.salary from Employee a where e.managerId = a.id
 )
