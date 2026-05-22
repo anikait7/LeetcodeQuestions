ef731fcbcfc99ce906434ea2c1210b7a8d4cd024
anikait7 <anikaitchitlangia1910@gmail.com>
2026-05-22 09:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (80.27%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,52 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int n = nums.length;
+
+        // Find smallest element index
+        int lb = 0;
+        int ub = n - 1;
+
+        while(lb < ub)
+        {
+            int mid = (lb + ub) / 2;
+
+            if(nums[mid] > nums[ub])
+                lb = mid + 1;
+            else
+                ub = mid;
+        }
+
+        int pivot = lb;
+
+        // Decide search range
+        int lb1, ub1;
+
+        if(target >= nums[pivot] && target <= nums[n - 1])
+        {
+            lb1 = pivot;
+            ub1 = n - 1;
+        }
+        else
+        {
+            lb1 = 0;
+            ub1 = pivot - 1;
+        }
+
+        // Binary search
+        while(lb1 <= ub1)
+        {
+            int mid = (lb1 + ub1) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                lb1 = mid + 1;
+            else
+                ub1 = mid - 1;
+        }
+
+        return -1;
+    }
+}
