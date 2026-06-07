b0dabf0302d96dafcc575518068f6d372e978bfa
anikait7 <anikaitchitlangia1910@gmail.com>
2026-06-07 16:29:55 +0530

    Time: 69 ms (43.13%) | Memory: 80.9 MB (85.41%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public TreeNode createBinaryTree(int[][] descriptions) 
+    {
+        Map<Integer,TreeNode> map = new HashMap<>();
+
+        for(int i=0;i<descriptions.length;i++)
+        {
+            TreeNode p;
+            TreeNode q = new TreeNode(descriptions[i][1]);
+
+            if(!map.containsKey(descriptions[i][0]))
+            {
+                p = new TreeNode(descriptions[i][0]);
+                map.put(descriptions[i][0],p);
+            }
+            else
+                p=map.get(descriptions[i][0]);
+            
+            if(!map.containsKey(descriptions[i][1]))
+            {
+                q = new TreeNode(descriptions[i][1]);
+                map.put(descriptions[i][1],q);
+            }
+            else
+                q=map.get(descriptions[i][1]);
+
+            if(descriptions[i][2]==1)
+                p.left=q;
+            else
+                p.right=q;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<descriptions.length;i++)
+            set.add(descriptions[i][0]);
+
+        for(int i=0;i<descriptions.length;i++)
+            set.remove(descriptions[i][1]);
+
+        return map.get(set.iterator().next());
+    }
+}
