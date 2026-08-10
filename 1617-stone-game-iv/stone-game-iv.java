5897bbd5b20c8c837e89d2e27932b159c58302f8
anikait7 <anikaitchitlangia1910@gmail.com>
2026-08-10 19:40:22 +0530

    Time: 41 ms (28.98%) | Memory: 49.5 MB (45.95%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,34 @@
+class Solution 
+{
+    int dp[];
+    List<Integer> list = new ArrayList<>();
+
+    public boolean recursive(int n)
+    {
+        if(n==0)
+            return false;
+
+        if(dp[n]!=-1)
+            return dp[n]==1;
+
+         for(int i=1;i*i<=n;i++)
+        {
+            if(recursive(n-i*i)==false)
+            {
+                dp[n]=1;
+                return true;
+            }
+        }     
+
+        dp[n]=0;
+        return false;
+    }
+
+    public boolean winnerSquareGame(int n)
+    {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return recursive(n);
+    }
+}
