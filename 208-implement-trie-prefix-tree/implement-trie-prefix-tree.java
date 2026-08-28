d1787c07147f082d74ffc05be39fafb5095347d9
anikait7 <anikaitchitlangia1910@gmail.com>
2026-08-29 01:41:14 +0530

    Time: 37 ms (30.02%) | Memory: 62.4 MB (23.01%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,3 +1,16 @@
+/*
+    This is the basics of TRIE. Most important question to understand the implementation
+    There is a root pointer that can point to 26 childs -> 'a' to 'z'
+    The second level again has 26 children each pointing from -> 'a' to 'z'
+    .........
+    These children are created when the letter comes otherwise the space will get wasted
+
+    apple and application
+    These share appl then brach to e,T and i,c,a.....(n,T)
+    T-> refers to word ending 
+    Due to the sharing of prefix, its saves lot of space and time. Very efficient
+*/
+
 class TrieNode
 {
     TrieNode child[] = new TrieNode[26];
